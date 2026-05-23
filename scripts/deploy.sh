6078576a22335a2cfeff58eae1b5c944079e78ee
solerdiaz <angelsd.dev@gmail.com>
2026-05-23 17:18:51 +0000

    Corregir despliegue continuo con nombre fijo de compose

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,32 @@
 #!/usr/bin/env bash
 set -e
 
+PROJECT_NAME="practica7-cicd-solerdiaz"
+
 echo "===== INICIO DESPLIEGUE AUTOMÁTICO ====="
 date
 
 cd "$(dirname "$0")/.."
 
 echo
+echo "===== PROYECTO DOCKER COMPOSE ====="
+echo "$PROJECT_NAME"
+
+echo
 echo "===== ACTUALIZAR IMAGEN DESDE REGISTRO ====="
-docker compose -f docker-compose.prod.yml pull || true
+docker compose -p "$PROJECT_NAME" -f docker-compose.prod.yml pull
 
 echo
 echo "===== LEVANTAR APLICACIÓN ====="
-docker compose -f docker-compose.prod.yml up -d
+docker compose -p "$PROJECT_NAME" -f docker-compose.prod.yml up -d --force-recreate
 
 echo
 echo "===== CONTENEDORES ACTIVOS ====="
 docker ps --filter "name=practica7_app"
+
+echo
+echo "===== IMAGEN USADA ====="
+docker inspect practica7_app --format='{{.Config.Image}}'
 
 echo
 echo "===== FIN DESPLIEGUE AUTOMÁTICO ====="
